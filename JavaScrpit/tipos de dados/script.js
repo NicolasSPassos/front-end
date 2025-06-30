c2b07e56029ff346aeae059846ede12eb6db54de
NicolasSPassos <nicolaspassos08@gmail.com>
2025-06-30 17:53:58 -0300

    mais atividades

---
+++ JavaScrpit/tipos de dados/script.js
@@ -0,0 +1,27 @@
+var nome = "Nicolas";
+var idade = 17;
+
+console.log(typeof idade);
+
+
+var frase1 = "Essa aula é 'muito' legal";
+
+var gols = 1000;
+var frase2 = `Romário fez ${gols * 5} gols`;
+
+console.log(frase2);
+
+var exercicio1 = "Aqui contém uma string",
+  exercicio2 = "25",
+  exercicio3 = 17;
+
+var nome = "Nicolas ",
+  sobrenome = "Passos",
+  nomecompleto = `${nome}${sobrenome}`;
+
+  var exercicio4 = "It's time"
+
+var verficartipo = typeof nome;
+
+  console.log(verficartipo);
+
---
+++ JavaScrpit/variaveis/script.js
@@ -0,0 +1,14 @@
+var nome = 'Nicolas',
+ idade = '17',
+ cor = 'Preto';
+
+var comidaFav
+comidaFav = 'Risoto'
+
+var livro, 
+professor, 
+rua,
+endereco,
+esporte;
+
+console.log(nome, idade, cor,comidaFav)
