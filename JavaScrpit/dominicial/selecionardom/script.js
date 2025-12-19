142a8d0c8a6b9e1192abe3fb53c9a193d1f67f0b
NicolasSPassos <nicolaspassos08@gmail.com>
2025-12-19 19:58:41 -0300

    ajustes e aprendizado sobre dom

---
+++ JavaScrpit/dominicial/selecionardom/script.js
@@ -0,0 +1,26 @@
+/*const animais = document.getElementById('animais');
+console.log(animais);*/
+
+const gridsection = document.getElementsByClassName('grid-section');
+
+console.log(gridsection[0]);
+
+const animais = document.querySelector('.animais');
+console.log(animais);
+
+const primeiroUl = document.querySelector('ul');
+
+const navItem = primeiroUl.querySelector('li');
+
+console.log(navItem);
+
+const linkInterno = document.querySelector('[href^="#"]');
+
+console.log(linkInterno);
+
+const alllistas = document.querySelectorAll('ul');
+console.log(alllistas);
+
+const allAnimais = document.querySelectorAll('.animais img');
+
+console.log(allAnimais);
