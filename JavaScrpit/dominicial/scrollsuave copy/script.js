7a3f7c0153aa659930dba1f993f85d0748b4aa8b
NicolasSPassos <nicolaspassos08@gmail.com>
2026-01-11 18:09:43 -0300

    ajustes e animação no scroll

---
+++ JavaScrpit/dominicial/scrollsuave copy/script.js
@@ -0,0 +1,106 @@
+function iniTabNav(){
+const tabMenu = document.querySelectorAll('.js-tabmenu li');
+
+const tabContent = document.querySelectorAll('.js-tabcontent section');
+tabContent[0].classList.add('ativo')
+
+if (tabMenu.length && tabContent){
+
+
+function ativarTab(index){
+  tabContent.forEach((section)=>{
+section.classList.remove('ativo')
+  });
+  tabContent[index].classList.add('ativo')
+}
+
+tabMenu.forEach((itemMenu, index)=>{
+itemMenu.addEventListener('click', ()=>{
+  ativarTab(index)
+})
+});
+ }
+}
+iniTabNav();
+
+function iniAccordion(){
+
+const accordionLista = document.querySelectorAll('.js-accordion dt')
+
+const classeAtivo = 'ativo';
+
+if(accordionLista.length){
+
+accordionLista[0].classList.add(classeAtivo)
+accordionLista[0].nextElementSibling.classList.add(classeAtivo)
+
+
+function ativarAccordion() {
+  this.classList.toggle(classeAtivo);
+  this.nextElementSibling.classList.toggle(classeAtivo);
+}
+
+
+
+accordionLista.forEach((item) =>{
+item.addEventListener('click', ativarAccordion )
+});
+}
+}
+
+iniAccordion();
+
+function iniScroll(){
+const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]')
+
+function scrollSuave(event){
+  event.preventDefault();
+  const href = event.currentTarget.getAttribute('href');
+  const section = document.querySelector(href)
+  
+section.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start',
+  })
+
+
+ /* const topo = section.offsetTop;
+ window.scrollTo({
+top: topo,
+behavior: 'smooth',
+  });*/
+}
+
+linksInternos.forEach((link) =>{
+link.addEventListener('click', scrollSuave)
+});
+}
+
+iniScroll();
+
+function iniAniScroll(){
+const sections = document.querySelectorAll('.js-scroll')
+
+if (sections.length){
+
+const meioWindow = window.innerHeight * 0.6;
+
+function animaScroll(){
+  sections.forEach((section) =>{
+const sectionsTop = section.getBoundingClientRect().top;
+const visuSection = (sectionsTop - meioWindow) < 0;
+if (visuSection){
+  section.classList.add('ativo')
+} else{
+  section.classList.remove('ativo')
+}
+  });
+  
+}
+animaScroll();
+
+window.addEventListener('scroll', animaScroll)
+}
+}
+
+iniAniScroll();
